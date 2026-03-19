4fdd6a8eac4c71b18c05ef5c444a87b6f1a21822
Jermain George <geojermain@gmail.com>
2026-03-19 22:56:47 +0300

    updated bd

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author':'Jermain Geo',
-        'Title':'Blog post 1',
-        'content':'All about code ' ,
-        'date_posted':'october 20th 2024', 
-
-    },
-    {
-        'author':'Jermain Geo',
-        'Title':'Blog post 2 ',
-        'content':'All about code2 ' ,
-        'date_posted':'october 21th 2024', 
-
-    }
-]
+from .models import Post
 
 def home(request):
 
-    context = {'posts':posts
+    context = {'posts':Post.objects.all()
                }
 
     return render (request, 'blog/home.html',context)
