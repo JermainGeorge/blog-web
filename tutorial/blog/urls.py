cba83bc2defbdbbae399c9aca05acfac0a37775c
Jermain George <geojermain@gmail.com>
2026-03-25 01:01:54 +0300

    user_registration_on_homepgT

--- tutorial/blog/urls.py
+++ tutorial/blog/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 from . import views
+from users import views as user_views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('login/', views.login, name='blog-login'),
+    path('register/', user_views.register, name='users-register'),
 
 ]
