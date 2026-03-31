67c7e7db388ebeba08a81ec16f9553dfbf6c933e
Jermain George <geojermain@gmail.com>
2026-04-01 00:04:26 +0300

    Profile creation step 1

--- tutorial/users/models.py
+++ tutorial/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#create a one to one relationship where one user has only one post 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
