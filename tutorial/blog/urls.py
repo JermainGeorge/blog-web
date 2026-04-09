1579b392a6fa254ccf9e3c6617098ed80a58a19b
Jermain George <geojermain@gmail.com>
2026-04-09 22:27:54 +0300

    profile complete

--- tutorial/blog/urls.py
+++ tutorial/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('user/<str:username>/', views.user_posts, name='user-posts'),
     
 ]
--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from .models import Post
 
 def home(request):
@@ -14,3 +15,14 @@ def about(request):
 
 def login(request):
     return render(request , 'blog/login.html', {'title':'Login'})
+
+
+def user_posts(request, username):
+    user = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=user)
+
+    context = {
+        'view_user': user,
+        'posts': posts
+    }
+    return render(request, 'blog/user_posts.html', context)
